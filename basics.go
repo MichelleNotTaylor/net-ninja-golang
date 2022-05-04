f638823c40f4884096e4b8254aa07da4d02ecd68
CodeMichelle <michelle.taylor@parchment.com>
2022-05-04 10:01:24 -0500

    Saving off basics in a separate file

---
+++ basics.go
@@ -0,0 +1,58 @@
+package main
+
+func main() {
+	// String
+	// var nameOne string = "Michelle"
+	// var nameTwo = "Derrick"
+	// var nameThree string
+
+	// fmt.Println(nameOne, nameTwo, nameThree)
+
+	// nameThree = "Ilana"
+	// nameFour := "Layla"
+	// fmt.Println(nameOne, nameTwo, nameThree, nameFour)
+
+	// // Integer
+	// var ageOne int = 27
+	// var ageTwo = 30
+	// ageThree := 2
+
+	// fmt.Println(ageOne, ageTwo, ageThree)
+
+	// // Bits and Memory
+	// var numOne int8 = 25
+	// var numTwo int8 = -128
+	// var numThree uint8 = 25
+
+	// fmt.Println(numOne, numTwo, numThree)
+
+	// var scoreOne float32 = 25.98
+	// var scoreTwo float64 = 219387120847019201.2
+	// scoreThree := 1.5
+
+	// fmt.Println(scoreOne, scoreTwo, scoreThree)
+
+	//Print
+	// age := "27"
+	// name := "Michelle"
+	// fmt.Print("Hello, ")
+	// fmt.Print("world!\n")
+	// fmt.Print("New line\n")
+
+	// //Println
+	// fmt.Println("Hello ninjas!")
+	// fmt.Println("Goodbye ninjas!")
+	// fmt.Println("My age is,", age, "and my name is", name)
+
+	// //Printf (formatted strings)
+	// fmt.Printf("My age is %v and my name is %v \n", age, name)
+	// fmt.Printf("My age is %q and my name is %q \n", age, name)
+	// fmt.Printf("My age is of type %T \n", age)
+	// fmt.Printf("You scored  %f points \n", 225.55)
+	// fmt.Printf("You scored %0.1f points! \n", 225.55)
+
+	// Sprintf (save formatted strings)
+	// var str = fmt.Sprintf("You scored %0.1f points! \n", 225.55)
+	// fmt.Println(str)
+
+}
