cb0bcb608244ad61b614db3adb7c1991ca5a34f4
CodeMichelle <michelle.taylor@parchment.com>
2022-05-09 22:02:53 -0500

    Final bill app with bill saving. Also some code playing with an interface.

--- main.go
+++ main.go
@@ -3,10 +3,45 @@ package main
 import (
 	"bufio"
 	"fmt"
+	"math"
 	"os"
 	"strconv"
 	"strings"
 )
+
+type shape interface {
+	area() float64
+	circumf() float64
+}
+
+type square struct {
+	length float64
+}
+
+type circle struct {
+	radius float64
+}
+
+func (s square) area() float64 {
+	return s.length * s.length
+}
+
+func (s square) circumf() float64 {
+	return s.length * 4
+}
+
+func (c circle) area() float64 {
+	return math.Pi * c.radius * c.radius
+}
+
+func (c circle) circumf() float64 {
+	return 2 * math.Pi * c.radius
+}
+
+func printShapeInfo(s shape) {
+	fmt.Printf("Area of %T is: %0.2f \n", s, s.area())
+	fmt.Printf("Circumference of %T is: %0.2f \n", s, s.circumf())
+}
 
 func getInput(prompt string, r *bufio.Reader) (string, error) {
 	fmt.Print(prompt)
@@ -64,6 +99,19 @@ func promptOptions(b bill) {
 }
 
 func main() {
+	// FOR INTERFACE TESTING
+	// shapes := []shape{
+	// 	square{length: 15.2},
+	// 	circle{radius: 7.5},
+	// 	circle{radius: 12.3},
+	// 	square{length: 4.9},
+	// }
+
+	// for _, v := range shapes {
+	// 	printShapeInfo(v)
+	// 	fmt.Println("---")
+	// }
+
 	myBill := createBill()
 	promptOptions(myBill)
 }
