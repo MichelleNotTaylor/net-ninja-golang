913b60d81c138f2dca9753e71a39d72be53d755f
CodeMichelle <michelle.taylor@parchment.com>
2022-05-09 21:47:33 -0500

    Parsing floats

--- main.go
+++ main.go
@@ -4,6 +4,7 @@ import (
 	"bufio"
 	"fmt"
 	"os"
+	"strconv"
 	"strings"
 )
 
@@ -27,7 +28,38 @@ func createBill() bill {
 func promptOptions(b bill) {
 	reader := bufio.NewReader(os.Stdin)
 	opt, _ := getInput("Choose an option (a - add item, s - save bill, t - add tip): ", reader)
-	fmt.Println(opt)
+
+	switch opt {
+	case "a":
+		name, _ := getInput("Item name: ", reader)
+		price, _ := getInput("Item price: ", reader)
+
+		p, err := strconv.ParseFloat(price, 64)
+		if err != nil {
+			fmt.Println("The price must be a number")
+			promptOptions(b)
+		}
+		b.addItem(name, p)
+		fmt.Println("Item added: ", name, price)
+		promptOptions(b)
+	case "t":
+		tip, _ := getInput("Enter top amount ($): ", reader)
+
+		t, err := strconv.ParseFloat(tip, 64)
+		if err != nil {
+			fmt.Println("The tip must be a number")
+			promptOptions(b)
+		}
+		b.updateTip(t)
+
+		fmt.Println("tip added - ", tip)
+		promptOptions(b)
+	case "s":
+		fmt.Println("You chose to save the bill", b)
+	default:
+		fmt.Println("That was not a valid option...")
+		promptOptions(b)
+	}
 }
 
 func main() {
