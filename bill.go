18ff678c56c1d9e472b4f07244cef8660a26c43b
CodeMichelle <michelle.taylor@parchment.com>
2022-05-09 21:21:27 -0500

    Receiver functions

--- bill.go
+++ bill.go
@@ -1,5 +1,8 @@
 package main
 
+import "fmt"
+
+// Struct
 type bill struct {
 	name  string
 	items map[string]float64
@@ -14,4 +17,27 @@ func newBill(name string) bill {
 	}
 
 	return b
+}
+
+func (b bill) format() string {
+	fs := "Bill breakdown: \n"
+	var total float64 = 0
+
+	for k, v := range b.items {
+		fs += fmt.Sprintf("%-25v ...$%v \n", k+":", v)
+		total += v
+	}
+
+	fs += fmt.Sprintf("%-25v ...$%v\n", "Tip:", b.tip)
+
+	fs += fmt.Sprintf("%-25v ...$%0.2f", "Total:", total+b.tip)
+	return fs
+}
+
+func (b *bill) updateTip(tip float64) {
+	b.tip = tip
+}
+
+func (b *bill) addItem(name string, price float64) {
+	b.items[name] = price
 }
--- main.go
+++ main.go
@@ -4,6 +4,9 @@ import "fmt"
 
 func main() {
 	myBill := newBill("Michelle's bill")
-	fmt.Println(myBill)
-
+	myBill.addItem("onion soup", 4.50)
+	myBill.addItem("chicken salad sandwich", 6.99)
+	myBill.addItem("summer salad", 5.00)
+	myBill.updateTip(10)
+	fmt.Println(myBill.format())
 }
